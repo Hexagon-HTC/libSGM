46ee7e7a9e6d1d11153b7cb7f9ce15ca3a96396e
adam-brzakala99 <116080867+adam-brzakala99@users.noreply.github.com>
2026-04-07 12:30:45 +0200

    Throw on error (#3)

--- src/host_utility.h
+++ src/host_utility.h
@@ -20,13 +20,27 @@ limitations under the License.
 #include <cstdio>
 #include <stdexcept>
 
-#define CUDA_CHECK(err)                                                                                        \
-    do                                                                                                         \
-    {                                                                                                          \
-        if (err != cudaSuccess)                                                                                \
-        {                                                                                                      \
-            printf("[CUDA Error] %s (code: %d) at %s:%d\n", cudaGetErrorString(err), err, __FILE__, __LINE__); \
-        }                                                                                                      \
+#define CUDA_CHECK(err)                                                                                                                                                          \
+    do                                                                                                                                                                           \
+    {                                                                                                                                                                            \
+        const cudaError_t cuda_check_err_ = (err);                                                                                                                               \
+        if (cuda_check_err_ != cudaSuccess)                                                                                                                                      \
+        {                                                                                                                                                                        \
+            char cuda_check_buf_[256];                                                                                                                                           \
+            snprintf(cuda_check_buf_, sizeof(cuda_check_buf_), "[CUDA Error] %s (code: %d) at %s:%d", cudaGetErrorString(cuda_check_err_), cuda_check_err_, __FILE__, __LINE__); \
+            printf("%s\n", cuda_check_buf_);                                                                                                                                     \
+            throw std::runtime_error(cuda_check_buf_);                                                                                                                           \
+        }                                                                                                                                                                        \
+    } while (0)
+
+#define CUDA_CHECK_NOTHROW(err)                                                                                                        \
+    do                                                                                                                                 \
+    {                                                                                                                                  \
+        const cudaError_t cuda_check_err_ = (err);                                                                                     \
+        if (cuda_check_err_ != cudaSuccess)                                                                                            \
+        {                                                                                                                              \
+            printf("[CUDA Error] %s (code: %d) at %s:%d\n", cudaGetErrorString(cuda_check_err_), cuda_check_err_, __FILE__, __LINE__); \
+        }                                                                                                                              \
     } while (0)
 
 #define SGM_ASSERT(expr, msg)        \
